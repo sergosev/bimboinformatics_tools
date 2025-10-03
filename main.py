4675d2c6ed458821bd33ea6f508256370323ce93
Sergey Losev <sergey.losev.01@gmail.com>
2025-10-03 12:40:24 +0300

    Set modules path in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#main python script
+import sys
+import os
+
+
+# Setting the paths to modules folder
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+modules_path = os.path.join(script_dir, "modules")
+sys.path.append(modules_path)
+
+
+from is_nucleic_acid import is_nucleic_acid
